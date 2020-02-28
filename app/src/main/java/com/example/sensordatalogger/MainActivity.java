e9fe73b0d0ffebe10fc87a6121b3af2959746d57
KevinD <kevind4272@yahoo.com>
2020-02-28 15:59:57 -0500

    Added runtime user permission request. Added dialog box on back button if recording.

--- app/src/main/java/com/example/sensordatalogger/MainActivity.java
+++ app/src/main/java/com/example/sensordatalogger/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.sensordatalogger;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int PERMISSION_REQUEST_CODE = 42;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,PERMISSIONS_STORAGE, PERMISSION_REQUEST_CODE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/sensordatalogger/SensorDisplay.java
+++ app/src/main/java/com/example/sensordatalogger/SensorDisplay.java
@@ -2,6 +2,7 @@ package com.example.sensordatalogger;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,6 +14,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -71,10 +74,37 @@ public class SensorDisplay extends Activity {
 
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sensorDataList.clear();
-                assert sensorManager != null;
-                sensorManager.unregisterListener(mySensorEventListener);
-                finish();
+                if (recording){
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which){
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    recordingText.setVisibility(View.INVISIBLE);
+                                    recording = false;
+                                    sensorDataList.clear();
+                                    assert sensorManager != null;
+                                    sensorManager.unregisterListener(mySensorEventListener);
+                                    finish();
+                                    break;
+
+                                case DialogInterface.BUTTON_NEGATIVE:
+
+                                    break;
+                            }
+                        }
+                    };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                    builder.setMessage("Do you want to stop recording?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+                } else {
+                    sensorDataList.clear();
+                    assert sensorManager != null;
+                    sensorManager.unregisterListener(mySensorEventListener);
+                    finish();
+                }
+
             }
         });
 
